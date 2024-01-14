cdba1a4615ec24993666565b3eeb09b0c004993d
GHOST <ghostdevbusiness@gmail.com>
2024-01-14 01:35:33 +0000

    feat: improve installation process

--- lib/glib.lua
+++ lib/glib.lua
@@ -1,6 +1,25 @@
-local command = ...
+local VERSION = "0.3.0"
 
-local VERSION = "0.2.0"
+local GLIB_COMMANDS = {
+    "glib"
+}
+
+local GLIB_TURTLE_COMMANDS = {
+    "fuel"
+}
+
+local function downloadFile(url, dest)
+    local file = fs.open(dest, "w")
+    
+    local response = http.get(url)
+    local data = response.readAll()
+    
+    file.write(data)
+    file.close()
+end
+
+local command = ...
+local isTurtle = turtle ~= nil
 
 if not command or command == "help" then
     print("GLib v" .. VERSION)
@@ -13,29 +32,28 @@ if not command or command == "help" then
     print("    version            Get the current version of GLib")
     print("    install, update    (re)install GLib")
 elseif command == "install" or command == "update" then
-    print("Installing GLib...")
+    term.clear()
+    print("Installing GLib:")
 
-    local files = {
-        "fuel",
-        "glib"
-    }
-
-    for i, file in ipairs(files) do
-        print("  Getting " .. file .. "...")
-
-        local fPath = "/" .. file .. ".lua"
-
-        fs.delete(fPath)
-
-        local response = http.get("https://raw.githubusercontent.com/ghostdevv/cc-glib/main/lib" .. fPath)
-        local file = fs.open(fPath, "w")
-        local data = response.readAll()
-
-        file.write(data)
-        file.close()
+    local function downloadCommands(commands)
+        for i, command in ipairs(commands) do
+            print("  - " .. command)
+    
+            local fPath = "/" .. command .. ".lua"
+    
+            fs.delete(fPath)
+            downloadFile("https://raw.githubusercontent.com/ghostdevv/cc-glib/main/lib" .. fPath, fPath)
+        end
     end
 
-    print("GLib v" .. VERSION .. " installed!")
+    downloadCommands(GLIB_COMMANDS)
+
+    if isTurtle then
+        print("\nInstalling GLib Turtle:")
+        downloadCommands(GLIB_TURTLE_COMMANDS)
+    end
+
+    print("\nGLib v" .. VERSION .. " installed!")
 elseif command == "version" then
     print("GLib v" .. VERSION)
 end
